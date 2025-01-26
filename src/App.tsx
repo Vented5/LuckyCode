fc41ad873d3f466db2e1bce3ccce4f5bd51d48e7
Vented5 <venteddiagram@gmail.com>
2025-01-26 00:35:21 -0500

    feat(home):Added functionality to home button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
 import "./App.css";
 import { AnimatedBg } from "./AnimatedBg";
+import { NavBar } from "./NavBar";
 import toothlessDancing from "./assets/toothless-dancing.gif";
+
 
 function App() {
   const [count, setCount] = useState(0);
+  const [dragonsito, setDragonsito] = useState<JSX.Element[]>([]);
+
+  let newDragonsito = <img className="flex m-auto mt-10" src={toothlessDancing} alt="" />
+
+  const addDragonsito = () => {
+    setCount((count) => count + 1)
+    setDragonsito((prevDragonsito) => [...prevDragonsito, newDragonsito]); 
+    console.log(dragonsito.length)
+  }
+  
+  
 
   return (
     <>
+      <NavBar/>
       <AnimatedBg>
         <h1>Lucky Code {"<X"}</h1>
         <p>
           Welcome tho the Anhedonia field! :D. where all my slavery comes true.
         </p>
-        <img className="flex m-auto mt-10" src={toothlessDancing} alt="" />
+
+        <section className="flex">
+          { dragonsito.map((dragon) => <>{dragon}</>) }
+        </section>
+        
+        
 
         <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
+          <button onClick={() => addDragonsito()}>
             count is {count}
           </button>
         </div>
 
         <h2>Heeeey weeeenas! que tal le va?</h2>
-        <p>Click on the Vite and React logos to learn more</p>
       </AnimatedBg>
     </>
   );
---
+++ src/NavBar.tsx
@@ -0,0 +1,6 @@
+export function NavBar () {
+    
+    return <section className="bg-lime-800">
+        LuckyCode
+    </section>
+}
